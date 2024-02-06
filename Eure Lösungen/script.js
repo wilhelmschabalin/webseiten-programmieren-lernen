11136c6ae89fbedb92e08d3e8048ad7ea8930a44
Garry the III <67372304+GarrytheIII@users.noreply.github.com>
2024-02-06 16:37:48 +0100

    Delete Eure Lösungen/script.js

--- Eure Lösungen/script.js
+++
@@ -1,3 +0,0 @@
-function Hello() { // This function is called when the button is clicked.
-    document.body.innerHTML = "Hello, World!" // When the function is called, the whole content of the body is replaced.
-}

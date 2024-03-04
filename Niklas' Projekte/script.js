b8f71ad68efbc2f7928197abc77a50dbbd331478
GarrytheIII <niklas.edinger@gmail.com>
2024-03-04 20:19:03 +0100

    Code hereinkopiert

---
+++ Niklas' Projekte/script.js
@@ -0,0 +1,43 @@
+function displayZero() {
+    document.getElementById("display-wrapper").innerHTML += "0";
+}
+
+function displayOne() {
+    document.getElementById("display-wrapper").innerHTML += "1";
+}
+
+function displayTwo() {
+    document.getElementById("display-wrapper").innerHTML += "2";
+}
+
+function displayThree() {
+    document.getElementById("display-wrapper").innerHTML += "3";
+}
+
+function displayFour() {
+    document.getElementById("display-wrapper").innerHTML += "4";
+}
+
+function displayFive() {
+    document.getElementById("display-wrapper").innerHTML += "5";
+}
+
+function displaySix() {
+    document.getElementById("display-wrapper").innerHTML += "6";
+}
+
+function displaySeven() {
+    document.getElementById("display-wrapper").innerHTML += "7";
+}
+
+function displayEight() {
+    document.getElementById("display-wrapper").innerHTML += "8";
+}
+
+function displayNine() {
+    document.getElementById("display-wrapper").innerHTML += "9";
+}
+
+function clearDisplay() {
+    document.getElementById("display-wrapper").innerHTML = "";
+}  

67fec4e9cf12902b2f43be0ec32da5e99d0856a8
GarrytheIII <niklas.edinger@gmail.com>
2024-03-04 21:25:11 +0100

    - script.js stark vereinfacht 	 - großes Danke an Willi  - Buttons für Rechenoperatoren hinzugefügt 	 - nur + und *, weil alle Mathematiker und Informatiker wissen, dass / und - nur Illusionen sind  - Buttons umpositioniert 	 - an unseren Casio-Taschenrechnern orientiert 	 - dabei auch noch ein Komma hinzugefügt

--- Niklas' Projekte/script.js
+++ Niklas' Projekte/script.js
@@ -1,41 +1,11 @@
-function displayZero() {
-    document.getElementById("display-wrapper").innerHTML += "0";
-}
+function display(content) {
+    const displayWrapper = document.getElementById("display-wrapper");
 
-function displayOne() {
-    document.getElementById("display-wrapper").innerHTML += "1";
-}
-
-function displayTwo() {
-    document.getElementById("display-wrapper").innerHTML += "2";
-}
-
-function displayThree() {
-    document.getElementById("display-wrapper").innerHTML += "3";
-}
-
-function displayFour() {
-    document.getElementById("display-wrapper").innerHTML += "4";
-}
-
-function displayFive() {
-    document.getElementById("display-wrapper").innerHTML += "5";
-}
-
-function displaySix() {
-    document.getElementById("display-wrapper").innerHTML += "6";
-}
-
-function displaySeven() {
-    document.getElementById("display-wrapper").innerHTML += "7";
-}
-
-function displayEight() {
-    document.getElementById("display-wrapper").innerHTML += "8";
-}
-
-function displayNine() {
-    document.getElementById("display-wrapper").innerHTML += "9";
+    if (content === "") {
+        displayWrapper.innerHTML = "";
+    } else {
+        displayWrapper.innerHTML += content;
+    }
 }
 
 function clearDisplay() {

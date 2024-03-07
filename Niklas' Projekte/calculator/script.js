f21ccc18dc62fd7b7f156d1ced468633d830e4bc
GarrytheIII <niklas.edinger@gmail.com>
2024-03-07 20:53:26 +0100

    - Kommentare zu script.js hinzugefügt
     - status-wrapper hinzugefügt
    	 - ganz cooles Gimmick, das ich hinzufügen wollte
    	 - dient außerdem zum Testen, in welchem Status der Code sich befindet
    
     - solution in String umgewandelt
    	 - verhindert z. B., dass die Eingabe von "2", dann "3", dann "=" 5 ausgibt --> gibt jetzt "23" aus
    	 - bis jetzt also nur Wiedergabe der Eingabe --> wirkliche Berechnung noch notwendig
    
     - status mit "running" und "complete" in den jeweiligen Farben gefüllt
    	 - im Moment noch nebeneinander, später einmal übereinander gedacht

--- Niklas' Projekte/calculator/script.js
+++ Niklas' Projekte/calculator/script.js
@@ -1,35 +1,74 @@
 let prohib = ['+', '*', '=']
 let solved = false
+let solution = ""
+
+function statuss() {
+    if (solved == false) {
+        document.getElementById("red").innerHTML = "running"
+        document.getElementById("green").innerHTML = ""
+    } else {
+        document.getElementById("green").innerHTML = "complete"
+        document.getElementById("red").innerHTML = ""
+    }
+}
+
+// display(content) shows whatever is entered.
 
 function display(content) {
     const displayWrapper = document.getElementById("display-wrapper")
 
-    if (solved == true) {
+    statuss()
+
+    if (solved == true) { // If the last process is solved and a button is pressed, the screen resets and the new process counts as not solved.
         displayWrapper.innerHTML = ""
         solved = false
     }
 
-    if (content === "") {
+    if (content == "") { // If the function is called with no parameter passed, nothing is displayed.
         displayWrapper.innerHTML = ""
-    } else if (prohib.includes(content) && prohib.includes(displayWrapper.innerHTML.slice(-1))) {
+    } else if (prohib.includes(content) && prohib.includes(displayWrapper.innerHTML.slice(-1))) { // If two operators are placed in a row, nothing will happen.
 
     } else {
         displayWrapper.innerHTML += content
+        solution += content
     }
+
+    statuss()
 }
 
+// clearDisplay() empties the display-wrapper.
+
 function clearDisplay() {
+
+    statuss()
+
     document.getElementById("display-wrapper").innerHTML = ""
+    solution = ""
+    solved = true
+
+    statuss()
 }
+
+// calculate() checks if the input is valid and, if so, calculates the result.
 
 function calculate() {
     const displayWrapper = document.getElementById("display-wrapper")
-    
-    if (prohib.includes(displayWrapper.innerHTML.slice(-1))) {
+
+    statuss()
+
+    if (prohib.includes(displayWrapper.innerHTML.slice(-1)) || prohib.includes(displayWrapper.innerHTML.slice(0, 1))) { // If the expression starts or ends with an operator, a syntax error is thrown.
         document.getElementById("display-wrapper").innerHTML = "syntax error"
         solved = true
-    } else {
-        document.getElementById("display-wrapper").innerHTML = "solution"
+        solution = ""
+    } else if (displayWrapper.innerHTML == "syntax error") { // If there is already a syntax error present, pressing the "=" button should not show a solution.
+
+    } else if (solution == "") { // If no solution could be calculated, nothing happens.
+
+    } else { // Else, the solution is displayed and the process counts as solved.
+        document.getElementById("display-wrapper").innerHTML = "'" + solution + "'"
         solved = true
+        solution = ""
     }
+
+    statuss()
 }

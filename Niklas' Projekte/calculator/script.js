b00a4dff75adc5f471b15007f134d703002e42d5
GarrytheIII <niklas.edinger@gmail.com>
2024-03-09 12:07:52 +0100

    - simpllified function strucuture in script.js  - modernized look and feel of UI

--- Niklas' Projekte/calculator/script.js
+++ Niklas' Projekte/calculator/script.js
@@ -3,6 +3,17 @@ let solved = false;
 let solution = "";
 let operator = null;
 
+function clickButton(input) {
+  showStatus();
+  if (input == "=") {
+    calculateResult();
+  } else {
+    displayInput(input);
+    saveInput(input);
+  }
+  showStatus();
+}
+
 function showStatus() {
   if (solved == false) {
     document.getElementById("red").innerHTML = "running";
@@ -13,38 +24,8 @@ function showStatus() {
   }
 }
 
-function display(content) {
-  const displayWrapper = document.getElementById("display-wrapper");
-  showStatus();
-  if (solved == true) {
-    // resets the screen after a process counts as solved
-    displayWrapper.innerHTML = "";
-    solved = false;
-  }
-  if (content === "") {
-    displayWrapper.innerHTML = "";
-  } else if (
-    prohib.includes(content) &&
-    prohib.includes(displayWrapper.innerHTML.slice(-1))
-  ) {
-    // No two operators or commas can be placed in a row.
-  } else {
-    displayWrapper.innerHTML += content;
-    solution += content;
-    if (content == "+" || content == "*") {
-      saveOperator(content);
-    }
-  }
-  showStatus();
-}
-
-function saveOperator(input) {
-  operator = input;
-}
-
 function calculateResult() {
   const displayWrapper = document.getElementById("display-wrapper");
-  showStatus();
   if (
     prohib.includes(displayWrapper.innerHTML.slice(-1)) ||
     prohib.includes(displayWrapper.innerHTML.slice(0, 1))
@@ -55,13 +36,40 @@ function calculateResult() {
   } else if (displayWrapper.innerHTML == "syntax error") {
     // prevents a solution from being displayed after a syntax error
   } else if (solution == "") {
-    // If no solution could be calculated, nothing happens.
   } else {
-    // Else, the solution is displayed and the process counts as solved.
     document.getElementById("display-wrapper").innerHTML = "'" + solution + "'";
     endProcess();
   }
-  showStatus();
+}
+
+function displayInput(input) {
+  const displayWrapper = document.getElementById("display-wrapper");
+  if (solved == true) {
+    // resets the screen after a process counts as solved
+    displayWrapper.innerHTML = "";
+    solved = false;
+  }
+  if (input === "") {
+    displayWrapper.innerHTML = "";
+  } else if (
+    prohib.includes(input) &&
+    prohib.includes(displayWrapper.innerHTML.slice(-1))
+  ) {
+    // prevents two operators or commas in a row
+  } else {
+    displayWrapper.innerHTML += input;
+  }
+}
+
+function saveInput(input) {
+  if (prohib.includes(input) && prohib.includes(solution.slice(-1))) {
+    // prevents two operators or commas in a row
+  } else if (input == "+" || input == "*") {
+    operator = input;
+    solution += input;
+  } else {
+    solution += input;
+  }
 }
 
 function clearDisplay() {
